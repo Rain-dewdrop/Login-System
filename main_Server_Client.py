5c84638fd59a73ce2a5d23a1a77e0a57e316e961
Rain_dewdrop <3350941618@qq.com>
2024-11-24 12:58:23 +0800

    Add files via upload

---
+++ main_Server_Client.py
@@ -0,0 +1,7 @@
+# import os
+# # 生成一个随机密钥（例如，用于AES-256）
+# key = os.urandom(32)  # AES-256需要一个256位的密钥
+# # 生成一个随机IV（初始化向量），对于AES，通常是128位
+# iv = os.urandom(16)  # AES需要一个128位的IV
+# print(iv)
+# print(key)
