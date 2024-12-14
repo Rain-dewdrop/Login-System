37a0f4b242e305bc9359a24cee4197eec01415cd
Rain_dewdrop <3350941618@qq.com>
2024-12-14 16:00:31 +0800

    Add files via upload

---
+++ Share/ClientRequestProcessing.py
@@ -0,0 +1,25 @@
+import definition as t
+
+def ClientRequestProcessing(command):
+    # 判断是否是验证登录信息的命令
+    if command.startswith("request_verify_login_password_") and "_nameword_" in command:
+        print(1)
+        return "1"
+    # 判断是否是获取服务端IV的命令
+    elif command == "request_get_iv":
+        print(2)
+        return "2"
+    # 判断是否是获取服务端key的命令
+    elif command == "request_get_key":
+        print(3)
+        return "3"
+    # 判断是否是获取到期时间的命令
+    elif command == "request_get_time":
+        print(4)
+        return "4"
+    elif command.startwith("request_verify_Activation_Code_"):
+        print(5)
+        return "5"
+    else:
+        t.timetips("Unknown command.")
+        return "Unknown command."
---
+++ Share/const.py
@@ -0,0 +1,3 @@
+Port_Number = 8888
+Max_Bytes = 1024
+SERVER_HOST = "127.0.0.1"
