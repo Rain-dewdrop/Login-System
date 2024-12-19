e25062064e7bc6885e9eb3150dbc90f7303a8135
Rain_dewdrop <3350941618@qq.com>
2024-12-19 20:25:20 +0800

    Add files via upload

--- main_Desktop_Client.py
+++ main_Desktop_Client.py
@@ -1,7 +1,12 @@
 import tkintertools.animation as animation
-import Share.definition as t
-import tkintertools as tkt
+from Share.b64 import *
 from Share.const import *
+from Share.message import *
+from Share.timetips import *
+from Share.notification import *
+from Share.HEX import *
+from Share.hwid import *
+import tkintertools as tkt
 import configparser
 import sys
 import os
@@ -20,22 +25,23 @@ LoginText = tkt.Text(cv, (450, 110), text="登 录", fontsize=25, anchor="center
 RegisterText = tkt.Text(cv, (450, -290), text="注  册", fontsize=25, anchor="center")
 username_input = tkt.InputBox(cv, (310, 180), (280, 40),placeholder="账号",  fontsize=20)
 password_input = tkt.InputBox(cv, (310, 240), (280, 40),placeholder="密码", show="*",  fontsize=20)
+tkt.IconButton(cv, (20, 20), text="IconButton", image=tkt.PhotoImage(file=os.path.join(directory_path,"Resource","Settings.png")).resize(32, 32))
 # 存储InputBox实例
-BackButton = tkt.Button(cv, (20, -200), text="<-Back", command=lambda:BackMainMenu())
+BackButton = tkt.Button(cv, (20, -200), text="<-返回", command=lambda:BackMainMenu())
 LoginButton = tkt.Button(cv, (310, 320), (170, 50), text="登录", fontsize=20,command=lambda:login_test(username_input, password_input))
 RegisterMenuButton = tkt.Button(cv, (490, 320), (100, 50), text="注册", fontsize=20,command=lambda:register_Menu())
 RegisterButton = tkt.Button(cv, (340, 820), (220, 50), text="注册", fontsize=20,command=lambda:print(1))
 tkt.Text(cv, (215, h-20), text="Different systems may have different visual presentations", fontsize=15, anchor="center")
-t.timetips("登录窗口组件加载完成")
+timetips("登录窗口组件加载完成")
 
 Overlay = tkt.Image(cv, (w/2, (h/2)*3),(w,h), image=tkt.PhotoImage(file=os.path.join(directory_path,"Resource","OverlayPhoto.png")), anchor="center")
 Overlay.disabled(True)
 
 def OverlayAnimation():
-    t.timetips('加载遮罩动画')
+    timetips('加载遮罩动画')
     Overlay.moveto(w/2, (h/2)*3)
-    animation.MoveComponent(Overlay, 800, (0, 0-h), controller=animation.smooth, fps=60).start(delay=100)
-    animation.MoveComponent(Overlay, 600, (w, 0), controller=animation.smooth, fps=60).start(delay=1500)
+    animation.MoveComponent(Overlay, 1000, (0, 0-h), controller=animation.smooth, fps=60).start(delay=100)
+    animation.MoveComponent(Overlay, 800, (w, 0), controller=animation.smooth, fps=60).start(delay=1500)
     
 def BackMainMenu():
     RegisterMenuButton.disabled(False)
@@ -59,20 +65,20 @@ def register_Menu():
 
 def login_test(username_input, password_input):
     global Login_status
-    t.timetips("尝试登录")
+    timetips("尝试登录")
     try:
         username = username_input.get()
         password = password_input.get()
-        hwid = t.get_hwid()
+        hwid = get_hwid()
         if 'Login' in config.sections() :
-            if t.HEX(username) == usernameHEX and t.HEX(password) == passwordHEX :
+            if HEX(username) == usernameHEX and HEX(password) == passwordHEX :
                 if not hwid == "" :
-                    if hwid == t.b64(config.get('Login', 'hwid')) :
-                        t.timetips("登录成功")
-                        t.send_notification("github.com/Rain-dewdrop/Login-System", "登录成功,欢迎回来！")
-                        username_b64 = t.b64code(username)
-                        password_b64 = t.b64code(password)
-                        hwid_b64 = t.b64code(hwid)
+                    if hwid == b64(config.get('Login', 'hwid')) :
+                        timetips("登录成功")
+                        send_notification("github.com/Rain-dewdrop/Login-System", "登录成功,欢迎回来！")
+                        username_b64 = b64code(username)
+                        password_b64 = b64code(password)
+                        hwid_b64 = b64code(hwid)
                         config['Login'] = {}
                         config['Login']['username'] = username_b64
                         config['Login']['password'] = password_b64
@@ -80,17 +86,17 @@ def login_test(username_input, password_input):
                         with open(config_file_path, 'w') as configfile:
                             config.write(configfile)
                         Login_status = True
-                        t.timetips("已更改登录状态")
+                        timetips("已更改登录状态")
                         root.destroy()
                     else:
-                        t.timetips("配置文件hwid验证失败")
-                        t.message("Hwid验证失败,请不要复制他人的配置文件,请在删除配置文件后重试!")
+                        timetips("配置文件hwid验证失败")
+                        message("Hwid验证失败,请不要复制他人的配置文件,请在删除配置文件后重试!")
                 else :
-                    t.timetips("登录成功！")
-                    t.send_notification("github.com/Rain-dewdrop/Login-System", "登录成功,欢迎回来！")
-                    username_b64 = t.b64code(username)
-                    password_b64 = t.b64code(password)
-                    hwid_b64 = t.b64code(hwid)
+                    timetips("登录成功！")
+                    send_notification("github.com/Rain-dewdrop/Login-System", "登录成功,欢迎回来！")
+                    username_b64 = b64code(username)
+                    password_b64 = b64code(password)
+                    hwid_b64 = b64code(hwid)
                     config['Login'] = {}
                     config['Login']['username'] = username_b64
                     config['Login']['password'] = password_b64
@@ -98,23 +104,23 @@ def login_test(username_input, password_input):
                     with open(config_file_path, 'w') as configfile:
                         config.write(configfile)
                     Login_status = True
-                    t.timetips("已更改登录状态")
+                    timetips("已更改登录状态")
                     root.destroy()
             else:
                 if username == "" or password == "":
-                    t.timetips("登录失败")
-                    t.message("请输入账号密码")
+                    timetips("登录失败")
+                    message("请输入账号密码")
                 else:
-                    t.timetips("登录失败")
-                    t.message("用户名或密码错误")
+                    timetips("登录失败")
+                    message("用户名或密码错误")
         else:
-            if t.HEX(username) == usernameHEX and t.HEX(password) == passwordHEX :
+            if HEX(username) == usernameHEX and HEX(password) == passwordHEX :
                 if not hwid == "" :
-                        t.timetips("登录成功！")
-                        t.send_notification("github.com/Rain-dewdrop/Login-System", "登录成功,欢迎回来")
-                        username_b64 = t.b64code(username)
-                        password_b64 = t.b64code(password)
-                        hwid_b64 = t.b64code(hwid)
+                        timetips("登录成功！")
+                        send_notification("github.com/Rain-dewdrop/Login-System", "登录成功,欢迎回来")
+                        username_b64 = b64code(username)
+                        password_b64 = b64code(password)
+                        hwid_b64 = b64code(hwid)
                         config['Login'] = {}
                         config['Login']['username'] = username_b64
                         config['Login']['password'] = password_b64
@@ -122,49 +128,49 @@ def login_test(username_input, password_input):
                         with open(config_file_path, 'w') as configfile:
                             config.write(configfile)
                         Login_status = True
-                        t.timetips("已更改登录状态")
+                        timetips("已更改登录状态")
                         root.destroy()
             else:
                 if username == "" or password == "":
-                    t.timetips("登录失败")
-                    t.message("请输入账号密码")
+                    timetips("登录失败")
+                    message("请输入账号密码")
                 else:
-                    t.timetips("登录失败")
-                    t.message("用户名或密码错误")
+                    timetips("登录失败")
+                    message("用户名或密码错误")
     except Exception as e:
-        t.timetips(str(e))
+        timetips(str(e))
 
 if config.read(config_file_path):
-    t.timetips("成功找到登录配置文件")
+    timetips("成功找到登录配置文件")
     if 'Login' in config.sections() and config.get('Login', 'username') != "" and config.get('Login', 'password') != "" and config.get('Login', 'hwid') != "":
         password_b64 = config.get('Login', 'password')
         username_b64 = config.get('Login', 'username')
-        t.timetips("成功解密配置文件")
-        password = t.b64(password_b64)
-        username = t.b64(username_b64)
+        timetips("成功解密配置文件")
+        password = b64(password_b64)
+        username = b64(username_b64)
         username_input.clear()
-        t.timetips("成功读取配置文件")
+        timetips("成功读取配置文件")
         password_input.clear()
-        t.timetips("已清空输入框内容")
+        timetips("已清空输入框内容")
         username_input.set(username)
         password_input.set(password)
-        t.timetips("已将输入框内容设置为配置文件内容")
+        timetips("已将输入框内容设置为配置文件内容")
     else:
-        t.timetips("配置文件错误")
+        timetips("配置文件错误")
 else:
-    t.timetips("配置文件不存在")
+    timetips("配置文件不存在")
     with open(config_file_path, 'w') as configfile:
         config.write(configfile)
-    t.timetips("配置文件初始化成功！")
+    timetips("配置文件初始化成功！")
 
 root.mainloop()
 
 #↑登陆界面  ↓功能界面
 
 if  Login_status == True:
-    t.timetips("登录状态验证成功")
+    timetips("登录状态验证成功")
 else:
-    t.timetips("登录状态验证失败")
+    timetips("登录状态验证失败")
     sys.exit()
 
 w = 650
--- main_Server_Client.py
+++ main_Server_Client.py
@@ -1,20 +1,19 @@
 import asyncio as a
-import Share.const as const
-import Share.definition as t
-
-
+from Share.const import *
+from Share.timetips import *
+from Share.ClientRequestProcessing import *
 
 async def handle_client(reader, writer):
-    data = await reader.read(const.Max_Bytes)
-    t.timetips(f"接收到客户端数据：{data}")
+    data = await reader.read(Max_Bytes)
+    timetips(f"接收到客户端数据：{data}")
     
     # 将字节数据解码为字符串，然后处理请求
     command = data.decode('utf-8')
-    response = t.ClientRequestProcessing(command)
+    response = ClientRequestProcessing(command)
     
     # 将处理结果编码为字节，然后发送给客户端
     writer.write(response.encode('utf-8'))
-    t.timetips(f"向客户端发送数据：{response}")
+    timetips(f"向客户端发送数据：{response}")
     
     await writer.drain()
     writer.close()
@@ -22,8 +21,8 @@ async def handle_client(reader, writer):
     return
 
 async def main():
-    sever = await a.start_server(handle_client,const.SERVER_HOST,const.Port_Number)
-    t.timetips(f"服务器已开启在 端口：{const.Port_Number}")
+    sever = await a.start_server(handle_client,SERVER_HOST,Port_Number)
+    timetips(f"服务器已开启在 端口：{Port_Number}")
     async with sever:
         await sever.serve_forever()
     return
